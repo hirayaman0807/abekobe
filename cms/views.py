58ac096c7ce892a800591e77ff12ed58b611b81f
nahomiyahira <nahomiyahira@gmail.com>
2020-05-11 10:32:02 +0900

    modified urls.py

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic.base import TemplateView
+
+
+class TopView(TemplateView):
+    template_name = 'cms/top.html'
