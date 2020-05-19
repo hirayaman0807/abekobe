06a970286c4865bf33eb302360ccdcd7ede9bea5
nahomiyahira <nahomiyahira@gmail.com>
2020-05-19 12:02:06 +0900

    #6

--- cms/forms.py
+++ cms/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django import forms
 
 UserModel = get_user_model()
 
@@ -15,6 +16,16 @@ class UserCreateForm(UserCreationForm):
     class Meta:
         model = UserModel
         fields = ('username', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'input'
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = UserModel
+        fields = ('username', 'first_name', 'last_name', 'email', 'twitter')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
