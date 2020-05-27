caa326536bdc89c4387a9380bfbc893abc421ba9
nahomiyahira <nahomiyahira@gmail.com>
2020-05-27 22:22:56 +0900

    chat機能追加

--- chat/urls.py
+++ chat/urls.py
@@ -4,6 +4,6 @@ from .views import *
 app_name = 'chat'
 urlpatterns = [
     path('', index, name='index'),
-    path('chat/<str:room_name>', chat, name='chat_room'),
+    path('chat_room/<str:room_name>', chat, name='chat_room'),
     path('room/', room, name='room'),
 ]
--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('cms.urls')),
-    path('', include('chat.urls', namespace='chat')), # 追加
+    path('chat/', include('chat.urls')), # 追加
 ]
